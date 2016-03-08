ce939bdbfd95721e17ba8caa6f33877d23ed99eb
Chris Vollink <chris@vollink.com>
2016-03-08 10:29:02 -0500

    Update 20160308093638_create_employees.rb

--- db/migrate/20160308093638_create_employees.rb
+++ db/migrate/20160308093638_create_employees.rb
@@ -2,7 +2,7 @@ class CreateEmployees < ActiveRecord::Migration
   def change
     create_table :employees do |t|
       t.string :name
-      t.integer :phone_number
+      t.string :phone_number
 
       t.timestamps null: false
     end
