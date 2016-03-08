ed9791f83ebc3bce659e522f15a62799fe2ed411
Christian Vollink <chris@vollink.com>
2016-03-08 16:04:26 -0500

    Seriously, this is getting old fast

--- db/migrate/20160308092552_create_jobs.rb
+++ db/migrate/20160308092552_create_jobs.rb
@@ -8,5 +8,13 @@ class CreateJobs < ActiveRecord::Migration
 
       t.timestamps null: false
     end
+    create_table :employees do |t|
+      t.string :name
+      t.string :phone_number
+
+      t.timestamps null: true
+    end
+
+    add_index :jobs, :employee_id
   end
 end
