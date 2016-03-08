9f7b84500cde4e8e001f01d8304e8eb634d89c98
Chris A. Vollink <chris@vollink.com>
2016-03-08 09:16:33 -0500

    Hopefully fixed tests.

--- test/models/employee_test.rb
+++ test/models/employee_test.rb
@@ -2,10 +2,7 @@ require 'test_helper'
 
 class EmployeeTest < ActiveSupport::TestCase
   def init
-    @employee = Employee.new(id: 1, name: "John Smith", phone_number: 5555555555)
-    @employee2 = Employee.new(id: 1, name: "John Doe", phone_number: "555\-555\-5555")
-    @employee3 = Employee.new(id: 1, name: "Janet Doe", phone_number: 555555555)
-    @employee4 = Employee.new(id: 1, name: "James Smith", phone_number: 55555555555)
+    @employee = Employee.create(id: 1, name: "John Smith", phone_number: 5555555555)
   end
 
   test "Check Employee Validity" do
@@ -13,15 +10,18 @@ class EmployeeTest < ActiveSupport::TestCase
   end
 
   test "Check Phone String Invalid" do
-    assert_not @employee2.valid?
+    @employee.update(phone_number: "555\-555\-5555")
+    assert_not @employee.valid?
   end
 
   test "Check 9 Digit Invalid" do
-    assert_not @employee3.valid?
+    @employee.update(phone_number: 555555555)
+    assert_not @employee.valid?
   end
 
   test "Check 11 Digit Invalid" do
-    assert_not @employee4.valid?
+    @employee.update(phone_number: 55555555555)
+    assert_not @employee.valid?
   end
 
 
--- test/models/job_test.rb
+++ test/models/job_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 class JobTest < ActiveSupport::TestCase
   def init
-    @employee = Employee.new(id: 1, name: "John Smith", phone_number: 5555555555)
-    @job_one = Job.new(id: 1, name: "Jane Doe", address: "1 Main Street, Springfield OH", datetime: DateTime.now)
+    @employee = Employee.create(id: 1, name: "John Smith", phone_number: 5555555555)
+    @job_one = Job.create(id: 1, name: "Jane Doe", address: "1 Main Street, Springfield OH", datetime: DateTime.now)
   end
 
   test "Check Job Validity" do
