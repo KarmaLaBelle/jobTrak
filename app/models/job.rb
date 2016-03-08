7e98ac65ffad1c3e52dd2d2c4d8dbef43e5c6ff3
Christian Vollink <chris@vollink.com>
2016-03-08 16:55:00 -0500

    This could be the one!

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,7 +2,6 @@ class Job < ActiveRecord::Base
   validates :name, presence: true
   validates :address, presence: true
   validates :phone_number, presence: true, uniqueness: true, format: { with: /\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}\z/ }
-  validates :job_time, presence: true
 
   belongs_to :employee
 end
--- test/models/job_test.rb
+++ test/models/job_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class JobTest < ActiveSupport::TestCase
   def setup
     @employee = Employee.create(id: 1, name: "John Smith", phone_number: "555-555-5555")
-    @job_one = Job.create(id: 1, name: "Jane Doe", address: "1 Main Street, Springfield OH", job_time: DateTime.now, employee_id: 1)
+    @job_one = Job.create(id: 1, name: "Jane Doe", address: "1 Main Street, Springfield OH", employee_id: 1)
   end
 
   test "Check Job Validity" do
@@ -11,6 +11,6 @@ class JobTest < ActiveSupport::TestCase
   end
 
   test "Check Job Count" do
-    assert_equal 1, @employee.jobs.count, "Not 1, you dumby"
+    assert_equal 1, Employee.first.jobs.count, "Not 1, you dumby"
   end
 end
