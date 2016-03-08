f4d93bf9412d10ee8a42940277b0e056e1cc6b3f
Christian Vollink <chris@vollink.com>
2016-03-08 16:49:53 -0500

    More test fixing. Now with overly less regex!!!

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,7 +2,7 @@ class Job < ActiveRecord::Base
   validates :name, presence: true
   validates :address, presence: true
   validates :phone_number, presence: true, uniqueness: true, format: { with: /\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}\z/ }
-  validates :jobtime, presence: true
+  validates :job_time, presence: true
 
   belongs_to :employee
 end
